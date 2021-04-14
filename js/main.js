09b0382b3f71e751f4435bccb21e8d96c2e3a028
IlyaBybnov <80475847+IlyaBybnov@users.noreply.github.com>
2021-04-14 10:31:33 +0300

    new site

---
+++ js/main.js
@@ -0,0 +1,15 @@
+jQuery(function($){
+    "use strict";
+
+    $('.content__video-link').magnificPopup({
+      disableOn: 0,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+    });
+    
+
+})
